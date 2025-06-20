30619e0bfefd58bbbeb0aa60246416bee9b5d50b
DosangE <zzoa0314@naver.com>
2025-06-20 15:47:52 +0900

    리소스 추가

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,16 +6,24 @@ public class PlayerHealth : MonoBehaviour
     [Header("플레이어 체력")]
     [SerializeField]
     public int health = 5;
-    
+
+    [Header("무적시간 (초)")]
+    [SerializeField] private float invincibleTime = 0.5f; // 깜빡임 애니메이션 길이에 맞춤
+
+    private Animator animator;
+    private bool isInvincible = false;
     public Action<int> OnHealthChanged;
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         OnHealthChanged?.Invoke(health);
     }
 
     public void TakeDamage(int damage)
     {
+        if (isInvincible) return; 
+        
         health -= damage;
         health = Mathf.Max(0, health);
 
@@ -23,12 +31,21 @@ public class PlayerHealth : MonoBehaviour
         {
             GameManager.instance.healthText.text = "HP: " + health;
         }
-
+        animator.SetTrigger("Hit");
+        StartCoroutine(InvincibleRoutine());
         if (health <= 0)
         {
             GameManager.instance.GameOver();
         }
-        
+
         OnHealthChanged?.Invoke(health);
+    }
+    private System.Collections.IEnumerator InvincibleRoutine()
+    {
+        isInvincible = true;
+
+        yield return new WaitForSeconds(invincibleTime); // 애니메이션 길이와 맞춤
+
+        isInvincible = false;
     }
 }
